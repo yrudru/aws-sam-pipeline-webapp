fa7744fc5d8730ab1a0b4a96ce15c9280643a7c5
YesuRaju <yrudru@my.bridgeport.edu>
2023-11-16 15:13:51 -0500

    text upadte

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "I am glad to have completed this tutorial.",
             # "location": ip.text.replace("\n", "")
         }),
     }
